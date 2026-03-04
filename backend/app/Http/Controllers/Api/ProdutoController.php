522a0e29d13c026fcfc55fcbce28c7812f62038a
jprdn <joao.nascimento@gmail.com>
2026-03-04 16:51:19 -0300

    Alteração backend

--- backend/app/Http/Controllers/Api/ProdutoController.php
+++ backend/app/Http/Controllers/Api/ProdutoController.php
@@ -12,7 +12,7 @@ use Exception;
 
 /*
     Controlador para endpoints da API de produtos.
-    Serviços de integração da API. Teste.
+    Serviços de integração da API. Teste 2.
 */
 
 class ProdutoController extends Controller
